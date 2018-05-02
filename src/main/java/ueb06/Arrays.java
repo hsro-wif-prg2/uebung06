c52d52b47e6b3884f2e23126bc6a4cef3eca1553
Anton <d1Av0lo3124@gmail.com>
2018-05-02 10:57:41 +0200

    --arrays_loesung

--- src/main/java/ueb06/Arrays.java
+++ src/main/java/ueb06/Arrays.java
@@ -5,13 +5,32 @@ class Arrays {
 	 * Gibt eine Stringrepraesentation des Arrays zurueck, z.B. [] oder [1, 2, 3]
 	 */
 	static <T> String toString(T[] array) {
-		return null;
-	}
 
+		if (array.length == 0) {
+            return "[ ]";
+        } else
+      return "[ " + toString(array,0) + " ]" ;}
+
+    static <T> String toString(T[] array, int a){
+        		if(a == array.length - 1)
+            			return array[a].toString();
+        	else
+        			return array[a].toString() + ", " + toString(array, a +1);
+    }
 	/**
 	 * Gib zurück, ob ein Objekt in dem Array enthalten ist; verwendet `equals`
 	 */
 	static <T> boolean contains(T[] array, T object) {
-		return false;
+
+	    return contains(array,object);
+	}
+
+	static <T> boolean contains(T[] array,T object,int a){
+	    if (a == array.length)
+            return false;
+	    else if(array[a].equals(object))
+	        return true;
+	    else
+	        return contains(array,object,a+1);
 	}
 }
